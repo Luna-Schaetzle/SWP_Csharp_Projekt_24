2344389b370961b2c7b9c7d428f49373d3261921
Luna-Schaetzle <Luna.Schaetzle@gmail.com>
2024-04-15 17:55:21 +0200

    Initalisierung des Projektordners DnD_Archive

---
+++ DnD_Archive/DnD_Archive/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DnD_Archive.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
