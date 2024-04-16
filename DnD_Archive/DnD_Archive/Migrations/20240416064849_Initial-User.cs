485a573c586e4a2853b0ec3867d9b239c4579d3d
Luna-Schaetzle <Luna.Schaetzle@gmail.com>
2024-04-16 08:49:23 +0200

    Update database connection string in DbManager.cs Migration for User

---
+++ DnD_Archive/DnD_Archive/Migrations/20240416064849_Initial-User.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DnD_Archive.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- DnD_Archive/DnD_Archive/Models/DB/DbManager.cs
+++ DnD_Archive/DnD_Archive/Models/DB/DbManager.cs
@@ -7,7 +7,7 @@ namespace DnD_Archive.Models.DB
     public class DbManager : Microsoft.EntityFrameworkCore.DbContext
     {
 
-        
+        public DbSet<User> Users { get; set; }
 
      
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -17,7 +17,7 @@ namespace DnD_Archive.Models.DB
             //      IP des MySQL-Servers
             //      Datenbankname ... selber angeben
             //      User/PWD ... die MySQL-Daten des Users mit dem sich der ORM bei MySQL anmelden soll
-            string connectionString = "Server=localhost;database=DnD_Archive;user=root;password=zwiebel55";
+            string connectionString = "Server=localhost;database=DND_Archive;user=root;password=zwiebel55";
             optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
     }
