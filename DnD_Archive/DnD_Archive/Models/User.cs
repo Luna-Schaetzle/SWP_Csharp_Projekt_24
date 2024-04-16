eec766614d5edad8cbc154fffe69d5c6cbb92bd3
jamati123 <fourtyeightcrash@gmail.com>
2024-04-16 08:43:20 +0200

    User

--- DnD_Archive/DnD_Archive/Models/User.cs
+++ DnD_Archive/DnD_Archive/Models/User.cs
@@ -4,13 +4,14 @@ namespace DnD_Archive.Models
     public class User
     {
         public int UserId { get; set; }
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string Email { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+      
 
         //Ctor's
-
+    
         //ToString()
 
 
