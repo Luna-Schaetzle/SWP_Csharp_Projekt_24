1b1dfe72939e3453f726d7ceb4100eb7a2c5bc38
Luna-Schaetzle <Luna.Schaetzle@gmail.com>
2024-04-23 10:45:41 +0200

    Füge Login- und Registrierungsseiten hinzu und aktualisiere das Start-Layout

--- DnD_Archive/DnD_Archive/Controllers/StartController.cs
+++ DnD_Archive/DnD_Archive/Controllers/StartController.cs
@@ -8,5 +8,15 @@ namespace DnD_Archive.Controllers
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registrieren()
+        {
+            return View();
+        }
     }
 }
